4388d1097cb0a11af7f964310826f9ff973fb4e4
jackson98-hub <80808495+jackson98-hub@users.noreply.github.com>
2023-07-18 12:39:04 -0500

    Add files via upload

---
+++ FinalProject/JavaApplication15/src/javaapplication15/JavaApplication15.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication15;
+
+/**
+ *
+ * @author JACKSON
+ */
+public class JavaApplication15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte edad;
+        edad=-127;
+        short aforo=20000;
+        System.out.println(aforo);
+    }
+    
+}
